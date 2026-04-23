0d4d0a29553b7fdb9a7ee172af3b6421b5374eb2
Jack SY Chen <jack_sy_chen@trendmicro.com>
2026-04-24 00:25:46 +0800

    refactor: remove i18n system and test pages

--- components/context/index.tsx
+++ components/context/index.tsx
@@ -1,13 +1,1 @@
 export { ThemeProvider, useTheme } from './ThemeContext';
-export { ThemeToggle } from './ThemeToggle';
-export {
-  LanguageProvider,
-  useLanguage,
-  useTranslation,
-  useTranslationSafe,
-  useLanguageSwitcher,
-  type Language,
-  type TranslationParams,
-  TranslationError
-} from './LanguageContext';
-export { LanguageToggle, SimpleLanguageToggle } from './LanguageToggle';
